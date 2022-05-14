281b257d03349e6759eec19019af7dc368977316
Dominic Francis <dominic.l.francis@outlook.com>
2022-05-14 18:01:15 +0800

    feat: validate choice fucntion

--- javascript.js
+++ javascript.js
@@ -17,12 +17,38 @@ function playRound() {
 function playerChoice () {
     //get input from the player
     let input = prompt('Type Rock, Paper, or Scissors');
+
+    while(input == null){
+        input = prompt('Type Rock, Paper or Scissors ')
+    }
+
+    input = input.toLowerCase();
+    //this makes the string all lower case
+    let check = validateInput(input)
+    if(check == true){
+        console.log(input);
+    }
+   // console.log(input);
 }
 
 function computerChoice() {
     //get random input from the computer
-    return choices[Math.floor(Math.random()*choices.length)]
+    return choices[Math.floor(Math.random() * choices.length)]
     //choice.length means 3, due to 3 choice 0,1,2, could input 3 as choices 
     //dont change
 }
 
+
+//below function validates the choice, makes sure 
+//input is R P or S
+//if not R P or S then it will not log the response
+function validateInput(choice){
+    if(choices.includes(choice)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//this begins the game
+game();
