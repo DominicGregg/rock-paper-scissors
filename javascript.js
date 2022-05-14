8d26bc6c8fe075517a0659f8b30cc88ea31e8a7b
Dominic Francis <dominic.l.francis@outlook.com>
2022-05-14 13:36:19 +0800

    feat: computer picks RPS

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//variable for R P and S
+const a = 'rock';
+const b = 'paper';
+const c = 'scissors';
+
+//randomly select an RPS variable 
+const array = [a, b, c];
+const rand = Math.floor(Math.random()*array.length);
+const rValue = array[rand];
+console.log(rValue);
+
+
+//computer plays RPC
+function computerPlay() {
+
+}
